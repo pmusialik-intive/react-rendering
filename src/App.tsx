3927466e961dce4f2135044783229cab07b6654a
Przemyslaw Musialik <pmusialik@blackberry.com>
2023-03-06 18:30:48 +0100

    Simplify App

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
 import './App.css'
-import Child from './components/family/Child'
-import Grandchild from './components/family/Grandchild'
-import Parent from './components/family/Parent'
 
 function App() {
   console.log('Rendering App...')
 
-  return (
-    <div className="App">
-      <Parent>
-        <Child>
-          <Grandchild></Grandchild>
-        </Child>
-      </Parent>
-    </div>
-  )
+  return <div className="App">React rendering</div>
 }
 
 export default App
