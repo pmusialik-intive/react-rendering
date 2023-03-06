16f03b6788b7358fa250e778db452fd581a8b802
Przemyslaw Musialik <pmusialik@blackberry.com>
2023-03-06 18:32:16 +0100

    Add rendering App info

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css'
 import ParentWithWholeFamily from './components/family/ParentWithWholeFamily'
 
 function App() {
+  console.log('Rendering App...')
+
   return (
     <div className="App">
       <ParentWithWholeFamily />
