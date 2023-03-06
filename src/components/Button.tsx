d5b7f8fc11ad357abc45895272deb782f82f495a
Przemyslaw Musialik <pmusialik@blackberry.com>
2023-03-06 17:47:22 +0100

    Add Button

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+interface Props {
+  onClick: () => void
+  text: string
+}
+
+const Button = (props: Props) => {
+  console.log('Rendering Button...')
+
+  return <button onClick={props.onClick}>{props.text}</button>
+}
+
+export default Button
