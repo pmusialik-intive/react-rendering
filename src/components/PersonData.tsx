e6a31de9ed8204cab71dcb40b34a3899776cef48
Przemyslaw Musialik <pmusialik@blackberry.com>
2023-03-06 18:13:32 +0100

    Add PersonData

---
+++ src/components/PersonData.tsx
@@ -0,0 +1,19 @@
+import { Person } from '../data/Person'
+
+interface Props {
+  person: Person
+}
+
+const PersonData = (props: Props) => {
+  console.log('Rendering PersonData...')
+
+  return (
+    <>
+      {Object.values(props.person).map((value, index) => (
+        <span key={`${index}_${value}`}>{value}</span>
+      ))}
+    </>
+  )
+}
+
+export default PersonData
