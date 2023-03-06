a45f8dfec7efea6b278e3640691b9cb5b782dbc4
Przemyslaw Musialik <pmusialik@blackberry.com>
2023-03-06 17:47:56 +0100

    Use family components at App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Child from './components/family/Child'
+import Grandchild from './components/family/Grandchild'
+import Parent from './components/family/Parent'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Parent>
+          <Child>
+            <Grandchild></Grandchild>
+          </Child>
+        </Parent>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
